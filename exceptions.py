f76010f057ae2ef16be6dd7a27726026cb6555c3
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2022-09-14 11:39:26 +1000

    Exceptions lesson

---
+++ exceptions.py
@@ -0,0 +1,38 @@
+class RangeError(Exception):
+    pass
+
+
+class InvalidUserError(Exception):
+    def __init__(self, name):
+        super().__init__(f'{name} is not a valid username')
+
+
+def get_int():
+    val = int(input('Enter an integer: '))
+    if not val in range(1, 11):
+        raise RangeError(f'{val} is out of range - must be between 1 and 10')
+
+    return val
+
+
+def get_username():
+    valid_users = ['alice', 'toby']
+    name = input('Username: ')
+    if not name in valid_users:
+        raise InvalidUserError(name)
+
+
+while True:
+    try:
+        x = get_int()
+        print(1 / x)
+        break
+    except RangeError as err:
+        print(err)
+    except ValueError:
+        print('Input must be an integer')
+    except ZeroDivisionError:
+        print('Input cannot be zero')
+
+
+print(f'You entered {x}')
