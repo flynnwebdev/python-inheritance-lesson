6f1cb2df276adf55c5a9e540f5cf3452dc3479a3
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2022-09-15 12:56:57 +1000

    Testing completed

--- test_exceptions.py
+++ test_exceptions.py
@@ -1,4 +1,23 @@
-from exceptions import get_int
+import pytest
+from exceptions import get_int, RangeError
+
+inputs = iter([5, 10, 20, -5])
+
+def fake_input(prompt):
+    return next(inputs)
 
 class TestGetInt:
-    pass
+    def test_valid(self, monkeypatch):
+        monkeypatch.setattr('builtins.input', fake_input)
+        assert get_int() == 5
+        assert get_int() == 10
+
+    def test_above_range(self, monkeypatch):
+        monkeypatch.setattr('builtins.input', fake_input)
+        with pytest.raises(RangeError):
+            get_int()
+
+    def test_below_range(self, monkeypatch):
+        monkeypatch.setattr('builtins.input', fake_input)
+        with pytest.raises(RangeError):
+            get_int()
